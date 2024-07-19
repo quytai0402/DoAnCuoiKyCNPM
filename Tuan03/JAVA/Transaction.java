97383e3c45f30d11bf358d7fafd326532e3f3a0c
Q T <tranquytai@MacBook-Pro-cua-Q.local>
2024-07-20 01:30:47 +0700

    Đồ án CNPM

---
+++ Tuan03/JAVA/Transaction.java
@@ -0,0 +1,2 @@
+// : Đây là lớp mô hình (model) đại diện cho một giao dịch của người dùng
+// . Nó chứa các thuộc tính date (ngày), description (mô tả), và amount (số tiền). Lớp này có các phương thức getter để truy cập các thuộc tính này.
